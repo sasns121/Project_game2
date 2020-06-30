1882517afda16dd26108fac262c932f88dae6e0e
sasns121 <56086154+sasns121@users.noreply.github.com>
2020-06-30 15:18:37 +0300

    2
    2

---
+++ Lessons/Assets/Scrpts/ActivateFlag.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ActivateFlag : MonoBehaviour
+{
+    public Animator Flag;
+    public Animator Char;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag=="Player")
+        {
+            PlayerMovement.CanMove = false;
+            Flag.enabled=true;
+            Char.Play("Player_finish");
+        }
+    }  
+}
---
+++ Lessons/Assets/Scrpts/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Transform target; //за кем следить 
+    [SerializeField] private float smoothSpeed; // скорость отставания
+    [SerializeField] private float MinX, MinY, MaxX, MaxY; //координаты предела
+
+    private void Start() //фуннкция выполняет дейсвие 1 раз при запуске
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void LateUpdate()//выполняе дейсвие после каждого кадра
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, -4.77f), smoothSpeed * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinX, MaxX),
+                                         Mathf.Clamp(transform.position.y, MinY, MaxY),
+                                         transform.position.z);
+    }
+}
